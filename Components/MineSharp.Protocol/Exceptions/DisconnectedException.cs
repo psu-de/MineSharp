88a0c58081f47c858b6514aa982926defdfb5307
qwqtoday <lukas.leung@hotmail.com>
2024-06-09 12:37:21 +0800

    Add disconnect exception

---
+++ Components/MineSharp.Protocol/Exceptions/DisconnectedException.cs
@@ -0,0 +1,10 @@
+﻿namespace MineSharp.Protocol.Exceptions;
+
+/// <inheritdoc />
+public class DisconnectedException(string message, string reason) : MineSharpHostException(message)
+{
+    /// <summary>
+    /// The reason of the disconnect
+    /// </summary>
+    public string Reason { get; private set; } = reason;
+}
