48468ddd1fd2b43853641768cc2ab3eb5b10b3bf
qwqtoday <lukas.leung@hotmail.com>
2024-07-08 16:58:55 +0800

    make reading title of window uses nbt text component in 1.20.3+

--- Components/MineSharp.Protocol/Packets/Clientbound/Play/OpenWindowPacket.cs
+++ Components/MineSharp.Protocol/Packets/Clientbound/Play/OpenWindowPacket.cs
@@ -1,3 +1,4 @@
+using MineSharp.ChatComponent;
 using MineSharp.Core.Common;
 using MineSharp.Data;
 using MineSharp.Data.Protocol;
@@ -11,14 +12,23 @@ public class OpenWindowPacket : IPacket
     public int    WindowId      { get; set; }
     public int    InventoryType { get; set; }
     public string WindowTitle   { get; set; }
-
+    
+    public Chat? WindowTitleChat { get; set; }
     public OpenWindowPacket(int windowId, int inventoryType, string windowTitle)
     {
         this.WindowId      = windowId;
         this.InventoryType = inventoryType;
         this.WindowTitle   = windowTitle;
     }
-
+    
+    public OpenWindowPacket(int windowId, int inventoryType, Chat windowTitle)
+    {
+        this.WindowId      = windowId;
+        this.InventoryType = inventoryType;
+        this.WindowTitle   = windowTitle.StyledMessage;
+        this.WindowTitleChat = windowTitle;
+    }
+    
     public void Write(PacketBuffer buffer, MinecraftData version)
     {
         buffer.WriteVarInt(this.WindowId);
@@ -28,6 +38,15 @@ public class OpenWindowPacket : IPacket
 
     public static IPacket Read(PacketBuffer buffer, MinecraftData version)
     {
+        if (version.Version.Protocol == ProtocolVersion.V_1_20_3)
+        {
+            return new OpenWindowPacket(
+                buffer.ReadVarInt(),
+                buffer.ReadVarInt(),
+                new Chat(buffer.ReadNbt()!, version)
+            );
+        }
+        
         return new OpenWindowPacket(
             buffer.ReadVarInt(),
             buffer.ReadVarInt(),
