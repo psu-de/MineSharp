1bf68a5879d91c9223578afe48f38a4088d41efe
Paul Sütterlin <68386188+psu-de@users.noreply.github.com>
2024-07-17 01:37:18 +0200

    fixed SystemChatMessagePacket.cs (#63)

--- Components/MineSharp.Protocol/Packets/Clientbound/Play/SystemChatMessagePacket.cs
+++ Components/MineSharp.Protocol/Packets/Clientbound/Play/SystemChatMessagePacket.cs
@@ -10,10 +10,10 @@ public class SystemChatMessagePacket : IPacket
 {
     public PacketType Type => PacketType.CB_Play_SystemChat;
 
-    public string Content   { get; set; }
-    public Chat? Message { get; set; }
-    public int?   ChatType  { get; set; }
-    public bool?  IsOverlay { get; set; }
+    public string? Content   { get; set; }
+    public Chat?   Message   { get; set; }
+    public int?    ChatType  { get; set; }
+    public bool?   IsOverlay { get; set; }
 
     /// <summary>
     /// Constructor for 1.19.1
@@ -31,49 +31,62 @@ public class SystemChatMessagePacket : IPacket
     /// </summary>
     /// <param name="content"></param>
     /// <param name="isOverlay"></param>
-    /// <param name="message"></param>
-    public SystemChatMessagePacket(string content, bool isOverlay, Chat? message = null)
+    public SystemChatMessagePacket(string content, bool isOverlay)
     {
-        this.Message = message;
         this.Content   = content;
         this.IsOverlay = isOverlay;
     }
+    
+    /// <summary>
+    /// Constructor for >= 1.20.3
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="isOverlay"></param>
     public SystemChatMessagePacket(Chat message, bool isOverlay)
     {
         this.Message = message;
-        this.Content = message.GetMessage(null); // TODO: check if content should really be set or it should maybe be null instead
         this.IsOverlay = isOverlay;
     }
 
     public void Write(PacketBuffer buffer, MinecraftData version)
     {
-        buffer.WriteString(this.Content);
+        if (version.Version.Protocol <= ProtocolVersion.V_1_20_2)
+        {
+            buffer.WriteString(this.Content 
+                            ?? throw new InvalidOperationException($"Content must be set for protocol version {version.Version.Protocol}"));
 
-        if (version.Version.Protocol >= ProtocolVersion.V_1_19_2)
-            buffer.WriteBool(this.IsOverlay!.Value);
-        else buffer.WriteVarInt(this.ChatType!.Value);
+            if (version.Version.Protocol == ProtocolVersion.V_1_19)
+            {
+                buffer.WriteVarInt(this.ChatType 
+                                ?? throw new InvalidOperationException($"ChatType must be set for protocol version {version.Version.Protocol}"));
+            }
+            else
+            {
+                buffer.WriteBool(this.IsOverlay 
+                              ?? throw new InvalidOperationException($"IsOverlay must be set for protocol version {version.Version.Protocol}"));
+            }
+
+            return;
+        }
+        
+        buffer.WriteChatComponent(this.Message 
+                               ?? throw new InvalidCastException($"Message must be set for protocol version {version.Version.Protocol}"));
+        buffer.WriteBool(this.IsOverlay 
+                      ?? throw new InvalidOperationException($"IsOverlay must be set for protocol version {version.Version.Protocol}"));
     }
 
     public static IPacket Read(PacketBuffer buffer, MinecraftData version)
     {
-        if (version.Version.Protocol < ProtocolVersion.V_1_20_3)
+        if (version.Version.Protocol > ProtocolVersion.V_1_20_2)
         {
-            var content = buffer.ReadString();
-            if (version.Version.Protocol >= ProtocolVersion.V_1_19_2)
-                return new SystemChatMessagePacket(content, buffer.ReadBool());
-
-            return new SystemChatMessagePacket(content, buffer.ReadVarInt());
-        } else
-        {
-            var content = buffer.ReadNbt();
-            try
-            {
-                return new SystemChatMessagePacket(buffer.ReadChatComponent(), buffer.ReadBool());
-            } catch
-            {
-                return new SystemChatMessagePacket(content!.ToString(), buffer.ReadBool());
-            }
+            return new SystemChatMessagePacket(buffer.ReadChatComponent(), buffer.ReadBool());
         }
+        
+        var content = buffer.ReadString();
+        return version.Version.Protocol >= ProtocolVersion.V_1_19_2 
+            ? new SystemChatMessagePacket(content, buffer.ReadBool()) 
+            : new SystemChatMessagePacket(content, buffer.ReadVarInt());
+
     }
 }
 #pragma warning restore CS1591
--- MineSharp.Bot.IntegrationTests/Program.cs
+++ MineSharp.Bot.IntegrationTests/Program.cs
@@ -1,42 +1,7 @@
-﻿using MineSharp.Bot;
-using MineSharp.Bot.IntegrationTests.Tests;
-using MineSharp.Bot.Plugins;
-using MineSharp.ChatComponent;
-using MineSharp.Data;
-using Newtonsoft.Json.Linq;
+﻿using MineSharp.Bot.IntegrationTests.Tests;
 
-var bot = await new BotBuilder()
-               .Host("localhost")
-               .OfflineSession("MineSharpBot")
-               .AutoConnect()
-               .CreateAsync();
-
-var chat = bot.GetPlugin<ChatPlugin>();
-chat.OnChatMessageReceived += (sender, player, message, position, name) =>
-{
-    Console.WriteLine($"Player={player}");
-    Console.WriteLine($"Message={message.GetMessage(bot.Data)}");
-    Console.WriteLine($"Position={position}");
-    Console.WriteLine($"Name={name}");
-};
-
-while (true)
-{
-    var msg = Console.ReadLine();
-
-    if (string.IsNullOrEmpty(msg))
-        continue;
-
-    if (msg == "exit")
-        break;
-    
-    await chat.SendChat(msg);
-}
-
-
-//
-// await PlayerTests.RunAll();
-// await WindowTests.RunAll();
-// await WorldTests.RunAll();
-// await CraftingTests.RunAll();
-// await PhysicsTests.RunAll();
+await PlayerTests.RunAll();
+await WindowTests.RunAll();
+await WorldTests.RunAll();
+await CraftingTests.RunAll();
+await PhysicsTests.RunAll();
