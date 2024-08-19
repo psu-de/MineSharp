dcab7031bb30dd246308df2269f117be090aed5e
Klotzi111 <Klotzi111@users.noreply.github.com>
2024-08-19 23:39:55 +0200

    fixed EntityEffectPacket contained types

--- Components/MineSharp.Protocol/Packets/Clientbound/Play/EntityEffectPacket.cs
+++ Components/MineSharp.Protocol/Packets/Clientbound/Play/EntityEffectPacket.cs
@@ -2,84 +2,89 @@
 using MineSharp.Core.Serialization;
 using MineSharp.Data;
 using MineSharp.Data.Protocol;
+using static MineSharp.Protocol.Packets.Clientbound.Play.EntityEffectPacket;
 
-namespace MineSharp.Protocol.Packets.Clientbound.Play
+namespace MineSharp.Protocol.Packets.Clientbound.Play;
+
+/// <summary>
+///     Represents the entity effect packet sent by the server to the client.
+/// </summary>
+/// <param name="EntityId">The entity ID</param>
+/// <param name="EffectId">The effect ID</param>
+/// <param name="Amplifier">The amplifier of the effect</param>
+/// <param name="Duration">The duration of the effect in ticks. -1 for infinity</param>
+/// <param name="Flags">The flags for the effect</param>
+/// <param name="HasFactorData">Indicates if the effect has factor data</param>
+/// <param name="FactorCodec">The factor codec data</param>
+public sealed record EntityEffectPacket(
+    int EntityId,
+    int EffectId,
+    byte Amplifier,
+    int Duration,
+    EffectFlags Flags,
+    bool HasFactorData,
+    NbtTag? FactorCodec) : IPacketStatic<EntityEffectPacket>
 {
-    /// <summary>
-    ///     Represents the entity effect packet sent by the server to the client.
-    /// </summary>
-    /// <param name="EntityId">The entity ID</param>
-    /// <param name="EffectId">The effect ID</param>
-    /// <param name="Amplifier">The amplifier of the effect</param>
-    /// <param name="Duration">The duration of the effect in ticks. -1 for infinity</param>
-    /// <param name="Flags">The flags for the effect</param>
-    /// <param name="HasFactorData">Indicates if the effect has factor data</param>
-    /// <param name="FactorCodec">The factor codec data</param>
-    public sealed record EntityEffectPacket(
-        int EntityId,
-        int EffectId,
-        byte Amplifier,
-        int Duration,
-        EffectFlags Flags,
-        bool HasFactorData,
-        NbtTag? FactorCodec) : IPacketStatic<EntityEffectPacket>
+    /// <inheritdoc />
+    public PacketType Type => StaticType;
+    /// <inheritdoc />
+    public static PacketType StaticType => PacketType.CB_Play_EntityEffect;
+
+    /// <inheritdoc />
+    public void Write(PacketBuffer buffer, MinecraftData data)
     {
-        /// <inheritdoc />
-        public PacketType Type => StaticType;
-        /// <inheritdoc />
-        public static PacketType StaticType => PacketType.CB_Play_EntityEffect;
-
-        /// <inheritdoc />
-        public void Write(PacketBuffer buffer, MinecraftData data)
+        buffer.WriteVarInt(EntityId);
+        buffer.WriteVarInt(EffectId);
+        buffer.WriteByte(Amplifier);
+        buffer.WriteVarInt(Duration);
+        buffer.WriteByte((byte)Flags);
+        buffer.WriteBool(HasFactorData);
+        if (HasFactorData)
         {
-            buffer.WriteVarInt(EntityId);
-            buffer.WriteVarInt(EffectId);
-            buffer.WriteByte(Amplifier);
-            buffer.WriteVarInt(Duration);
-            buffer.WriteByte((byte)Flags);
-            buffer.WriteBool(HasFactorData);
-            if (HasFactorData)
-            {
-                buffer.WriteNbt(FactorCodec!);
-            }
-        }
-
-        /// <inheritdoc />
-        public static EntityEffectPacket Read(PacketBuffer buffer, MinecraftData data)
-        {
-            var entityId = buffer.ReadVarInt();
-            var effectId = buffer.ReadVarInt();
-            var amplifier = buffer.ReadByte();
-            var duration = buffer.ReadVarInt();
-            var flags = (EffectFlags)buffer.ReadByte();
-            var hasFactorData = buffer.ReadBool();
-            NbtTag? factorCodec = null;
-            if (hasFactorData)
-            {
-                // TODO: Check if this is correct
-                factorCodec = buffer.ReadNbt();
-            }
-
-            return new EntityEffectPacket(
-                entityId,
-                effectId,
-                amplifier,
-                duration,
-                flags,
-                hasFactorData,
-                factorCodec);
-        }
-
-        static IPacket IPacketStatic.Read(PacketBuffer buffer, MinecraftData data)
-        {
-            return Read(buffer, data);
+            buffer.WriteNbt(FactorCodec!);
         }
     }
 
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+    /// <inheritdoc />
+    public static EntityEffectPacket Read(PacketBuffer buffer, MinecraftData data)
+    {
+        var entityId = buffer.ReadVarInt();
+        var effectId = buffer.ReadVarInt();
+        var amplifier = buffer.ReadByte();
+        var duration = buffer.ReadVarInt();
+        var flags = (EffectFlags)buffer.ReadByte();
+        var hasFactorData = buffer.ReadBool();
+        NbtTag? factorCodec = null;
+        if (hasFactorData)
+        {
+            // TODO: Check if this is correct
+            factorCodec = buffer.ReadNbt();
+        }
+
+        return new EntityEffectPacket(
+            entityId,
+            effectId,
+            amplifier,
+            duration,
+            flags,
+            hasFactorData,
+            factorCodec);
+    }
+
+    static IPacket IPacketStatic.Read(PacketBuffer buffer, MinecraftData data)
+    {
+        return Read(buffer, data);
+    }
+
+    /// <summary>
+    /// Effect flags used in the entity effect packet.
+    /// </summary>
     [Flags]
     public enum EffectFlags
     {
+        /// <summary>
+        /// No flags
+        /// </summary>
         None = 0,
         /// <summary>
         ///  Is ambient - was the effect spawned from a beacon?
@@ -102,5 +107,4 @@ namespace MineSharp.Protocol.Packets.Clientbound.Play
         /// </summary>
         ShowIcon = 0x04
     }
-#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 }
